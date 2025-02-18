5edadc6668b46d448acd92a193f82d540b5d4e0c
gardas123 <prashaantgardas64@gmail.com>
2025-02-18 21:12:24 +0530

    fixed bug

--- FIleHandling.py
+++ FIleHandling.py
@@ -1,13 +1,22 @@
 
+global a
+
 def Details():
+    global a
     firstname = input("Enter Name :")
     rollno = int(input("Enter Rollno"))
+    a = int(input("enter std :"))
 
     print("Name :",firstname)
     print("Rollno :",rollno)
+
+def show():
+    print("intger :",a)
 
 
 try:
     Details()
 except:
     print("enter correct details")
+
+show()
