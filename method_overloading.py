e10c30f5945fa6aa4d8c42572ab57af9f33e0346
gardas123 <prashaantgardas64@gmail.com>
2025-02-20 21:01:38 +0530

    method overloading program

---
+++ method_overloading.py
@@ -0,0 +1,16 @@
+
+# First product method.
+# Takes two argument and print their
+# product
+
+
+def product(a, b):
+    p = a * b
+    print(p)
+
+def product(a,b,c):
+    d = a * b * c
+    print(d)
+
+product(4, 5, 6)
+
