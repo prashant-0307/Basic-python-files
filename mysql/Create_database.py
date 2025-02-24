ebce0fef2cd9c1ceacef8b3bca7c68f01711cfc4
gardas123 <prashaantgardas64@gmail.com>
2025-02-24 21:32:08 +0530

    create database program

---
+++ mysql/Create_database.py
@@ -0,0 +1,9 @@
+import mysql.connector as mycon # type: ignore 
+
+db = mycon.connect(host = "localhost" , user = "root" , password = "Prashant@123")
+db_cursor = db.cursor()
+try:
+    db_cursor.execute("create database mypython")
+    print("database created successfully..........")
+except:
+    print("database already exsits")
